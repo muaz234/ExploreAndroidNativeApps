3b3e918ee092e545e481bf515db89ab7c735547d
muaz.ahmed <ahmedmuaz0152@gmail.com>
2019-07-07 17:31:10 +0800

    add github url and load them, pending to fix the toolbar size
    Signed-off-by: muaz.ahmed <ahmedmuaz0152@gmail.com>

--- app/src/main/java/com/muazlabs/webview/MainActivity.java
+++ app/src/main/java/com/muazlabs/webview/MainActivity.java
@@ -5,16 +5,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     WebView webview;
+    TextView title;
     String url = "https://github.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
+        title = findViewById(R.id.title);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Github");
         webview = findViewById(R.id.webview);
@@ -22,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         webview.clearCache(true);
         webview.clearHistory();
         try {
-//            webview.loadUrl(url);
-            webview.loadUrl("https://www.google.com/");
+            webview.loadUrl(url);
+//            webview.loadUrl("https://www.google.com/");
         } catch (Exception e) {
             webview.loadUrl("www.google.com");
             Log.i("error", e.toString());
