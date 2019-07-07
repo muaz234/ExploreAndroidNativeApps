1e721e494a70473c40ab27b9036801f31dc5c101
muaz.ahmed <ahmedmuaz0152@gmail.com>
2019-07-07 21:14:15 +0800

    add webviewclient
    Signed-off-by: muaz.ahmed <ahmedmuaz0152@gmail.com>

--- app/src/main/java/com/muazlabs/webview/MainActivity.java
+++ app/src/main/java/com/muazlabs/webview/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,15 +27,24 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         title = findViewById(R.id.title);
         setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setTitle("Github");
         webview = findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.clearCache(true);
         webview.clearHistory();
+        webview.setVerticalScrollBarEnabled(true);
+        webview.setHorizontalFadingEdgeEnabled(true);
+
         try {
+            webview.setWebViewClient(new WebViewClient());
+//           webview.setWebChromeClient(new WebChromeClient());
             webview.loadUrl(url);
+            //testing with google link
 //            webview.loadUrl("https://www.google.com/");
         } catch (Exception e) {
+            //load url google instead of any error message which user get confuse!!
             webview.loadUrl("www.google.com");
             Log.i("error", e.toString());
         }
